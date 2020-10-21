490df011fb75173eed7d959ae7bc4dd0512b5cb7
Clayton Gilbert <claytongilbert@gmail.com>
2020-10-21 13:23:00 +1100

    FormFrame initial

---
+++ Example/Classes/PrintVersion.swift
@@ -0,0 +1,23 @@
+//
+//  PrintVersion.swift
+//  FormFrame_Example
+//
+//  Created by Clayton on 21/10/20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+class PrintVersion{
+    
+    init() {
+       
+        printNumber()
+    }
+        
+    
+    func printNumber(){
+        print("1.0")
+    }
+    
+}
---
+++ Example/Pods/Target Support Files/FormFrame/FormFrame-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FormFrame : NSObject
+@end
+@implementation PodsDummy_FormFrame
+@end
